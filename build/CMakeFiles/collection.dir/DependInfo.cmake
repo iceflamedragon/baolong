5c4c4a7add24fe5474454c2a1d7b3da2ed665375
iceflamedragon <1466077990@qq.com>
2024-03-22 21:45:10 +0800

    祖传

---
+++ build/CMakeFiles/collection.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/edgeboard/car/demo/src/tool/collection.cpp" "/home/edgeboard/car/demo/build/CMakeFiles/collection.dir/tool/collection.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/edgeboard/car/demo/src/include"
+  "/home/edgeboard/car/demo/src/config"
+  "/home/edgeboard/car/demo/src/src"
+  "/home/edgeboard/car/demo/src/tool"
+  "/usr/include/glib-2.0"
+  "/usr/lib/aarch64-linux-gnu/glib-2.0/include"
+  "/usr/local/include/opencv4"
+  "/usr/local/include/libserial"
+  "/usr/local/include/ppnc"
+  "/usr/local/include/onnx"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
