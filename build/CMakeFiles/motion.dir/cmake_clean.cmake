dc17cec615ed76e20997017fc2ec15c915b88f0e
iceflamedragon <1466077990@qq.com>
2024-05-22 22:09:15 +0800

    尝试把motion改成h文件，失败

---
+++ build/CMakeFiles/motion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/motion.dir/src/motion.cpp.o"
+  "libmotion.pdb"
+  "libmotion.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/motion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
